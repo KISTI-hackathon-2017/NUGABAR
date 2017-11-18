e23719c07c7d5b0cd408c260bee33ff3b3f9291a
Yun <Yun@DESKTOP-N6CH1UB>
2017-11-18 11:47:19 +0900

    DataBase Connection

---
+++ src/database/DBConnection.java
@@ -0,0 +1,37 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBConnection {
+
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+
+	public DBConnection() {
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost/tutorial", "root", "root");
+			st = con.createStatement();
+		} catch (Exception e) {
+			System.out.println("Database Connection Error \n" + e.getMessage());
+		}
+	}
+
+	public boolean isAdmin(String adminID, String adminPassword) {
+		try {
+			String SQL = "SELECT * FROM ADMIN WHERE adminID = '" + adminID + "'and adminPassword = '" + adminPassword + "'";
+			rs = st.executeQuery(SQL);
+			if(rs.next())
+				return true;
+			else
+				return false;
+		} catch (Exception e) {
+			System.out.println("Search Data Error" + e.getMessage());
+			return false;
+		}
+	}
+
+}
